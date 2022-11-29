dbfc8fdf58853d25e68316c090c5c4a55726e0c8
Zach <86675944+zwagnr@users.noreply.github.com>
2022-11-29 12:34:38 -0500

    fix incorrect math formula (#3)

--- src/math.js
+++ src/math.js
@@ -1,6 +1,6 @@
 //Math Equations
 export const xPercentOfY = (x, y) => (x / 100) * y;
 export const xIsWhatPercentOfY = (x, y) => (x / y) * 100;
-export const xIsYPercentOf = (x, y) => x / y;
+export const xIsYPercentOf = (x, y) => x / (y / 100);
 export const percentIncrease = (x, y) => x * (1 + y / 100);
 export const percentDecrease = (x, y) => x * (1 - y / 100);
